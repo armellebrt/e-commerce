c7a7e2634d3a7d2d9bb65a21eb4e6aa3c59c5796
armellebrt <81526434+armellebrt@users.noreply.github.com>
2021-09-13 12:27:51 +0200

    Update footer.php

--- Application/View/backend/theme/footer.php
+++ Application/View/backend/theme/footer.php
@@ -3,7 +3,7 @@
         <p class="float-end mb-1">
             <a href="#">Back to top</a>
         </p>
-        <p class="mb-4"><a href="#"><i class="fab fa-github"></i> Lien Github du site</a> </p>
+        <p class="mb-4"><a href="https://github.com/armellebrt/e-commerce"><i class="fab fa-github"></i> Lien Github du site</a> </p>
         <p class="mb-4"><a href="https://armelle.mystagiaire.fr/"> <img src="./images/favicon-32x32.png" alt="icon-cv"> Lien CV</a></p>
         <p>Technos utilisées: PHP POO MVC</p>
     </div>
